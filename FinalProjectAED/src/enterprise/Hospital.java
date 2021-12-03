4fb51d4c42badb25224dc90861591e3f97018660
Harshhal jaiswal <harshaljaiswal25@gmail.com>
2021-12-03 11:03:23 -0500

    initial commit

---
+++ FinalProjectAED/src/enterprise/Hospital.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enterprise;
+
+import components.Address;
+
+/**
+ *
+ * @author harshaljaiswal
+ */
+public class Hospital extends Enterprise{
+    
+    public Hospital(String enterpriseName, String registeryNumber, Address address) {
+        super(enterpriseName, registeryNumber, address);
+    }
+    
+}
