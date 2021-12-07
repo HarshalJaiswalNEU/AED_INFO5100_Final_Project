0686c95b9e9d8e6d1db32c80e3791ea79221bcb8
Mayur Avinash Khandetod <khandetod.m@northeastern.edu>
2021-12-07 00:19:25 -0500

    Renamed files

--- AEDFinalProjectTry1/src/main/java/Business/Enterprise/Dispensary/Dispensary.java
+++ AEDFinalProjectTry1/src/main/java/Business/Enterprise/Dispensary/Dispensary.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Business.EcoSystem.Dispensary;
+package Business.Enterprise.Dispensary;
 
 import Business.Components.Address;
 import Business.Enterprise.Enterprise;
--- AEDFinalProjectTry1/src/main/java/Business/Enterprise/Lab/Lab.java
+++ AEDFinalProjectTry1/src/main/java/Business/Enterprise/Lab/Lab.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Business.EcoSystem.Sponsorer;
+package Business.Enterprise.Lab;
 
 import Business.Components.Address;
 import Business.Enterprise.Enterprise;
@@ -11,9 +11,9 @@ import Business.Enterprise.Enterprise;
  *
  * @author harshaljaiswal
  */
-public class Sponsor extends Enterprise{
+public class Lab extends Enterprise{
     
-    public Sponsor(String enterpriseName, String registeryNumber, Address address) {
+    public Lab(String enterpriseName, String registeryNumber, Address address) {
         super(enterpriseName, registeryNumber, address);
     }
     
