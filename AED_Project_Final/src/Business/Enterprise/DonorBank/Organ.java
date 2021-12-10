5b01fbdf04a868bc998b47c70476080e845f7bd7
MayurAvinash <91162861+MayurAvinash@users.noreply.github.com>
2021-12-09 21:59:57 -0500

    UI changes

---
+++ AED_Project_Final/src/Business/Enterprise/DonorBank/Organ.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise.DonorBank;
+
+/**
+ *
+ * @author Mayur
+ */
+public class Organ {
+    
+private String organname;
+
+    public String getOrganname() {
+        return organname;
+    }
+
+    public void setOrganname(String organname) {
+        this.organname = organname;
+    }
+
+
+}
