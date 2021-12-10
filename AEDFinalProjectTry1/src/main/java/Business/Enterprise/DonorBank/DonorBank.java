fdac334eaf8e0dac8ebccecf5745c697238ad3cd
HarshalJaiswalNEU <jaiswal.ha@northeastern.edu>
2021-12-09 19:17:49 -0500

    Revert "aed project try 5"
    This reverts commit b81f6e6541fae105c4c4ab54cffff13f11046460.

---
+++ AEDFinalProjectTry1/src/main/java/Business/Enterprise/DonorBank/DonorBank.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise.DonorBank;
+
+import Business.Components.Address;
+import Business.Enterprise.Enterprise;
+
+/**
+ *
+ * @author harshaljaiswal
+ */
+public class DonorBank extends Enterprise{
+    
+    public DonorBank(String enterpriseName, String registeryNumber, String address, String username, String password) {
+        super(enterpriseName, registeryNumber, address, username, password);
+    }
+    
+}
