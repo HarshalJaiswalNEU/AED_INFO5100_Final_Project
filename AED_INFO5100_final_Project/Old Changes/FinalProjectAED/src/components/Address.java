b05f0bc8adbd5cfb7cf08f444d7c67acf82e6563
HarshalJaiswalNEU <jaiswal.ha@northeastern.edu>
2021-12-22 16:50:30 -0500

    working project

---
+++ AED_INFO5100_final_Project/Old Changes/FinalProjectAED/src/components/Address.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package components;
+
+/**
+ *
+ * @author harshaljaiswal
+ */
+public class Address {
+    private String line1;
+    private String line2;
+    private int aptNo;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    public Address(String line1, String line2, int aptNo, String city, String state, int zipCode) {
+        this.line1 = line1;
+        this.line2 = line2;
+        this.aptNo = aptNo;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public int getAptNo() {
+        return aptNo;
+    }
+
+    public void setAptNo(int aptNo) {
+        this.aptNo = aptNo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+    
+    
+}
